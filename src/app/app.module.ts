8b8bff407b7dd274d2ee12c7113241a165b91575
Vanessa Oliveira <tauchertvanessa@gmail.com>
2022-11-24 02:38:59 -0300

    create user list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsuarioListComponent } from './components/usuario/usuario-list/usuario-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    UsuarioListComponent,
+   
   ],
   imports: [
     BrowserModule,
